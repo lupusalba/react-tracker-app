855bd5486307be07780f7cb424f548bb72a440da
lupusalba <a145296@gmail.com>
2022-04-02 20:26:35 +0200

    54min in tutorial

--- src/App.js
+++ src/App.js
@@ -9,26 +9,35 @@ function App() {
     {
       "id": 1,
       "text": "Lorem ipsum",
+      "day": "Apr 5th, at 11:00",
       "reminder": true,
     }, {
       "id": 2,
       "text": "consectetur adipiscing",
+      "day": "Apr 5th, at 11:00",
       "reminder": true,
     }, {
       "id": 3,
       "text": "dolor sit amet",
+      "day": "Apr 5th, at 11:00",
       "reminder": true,
     }, {
       "id": 4,
       "text": "Lorem ipsum",
+      "day": "Apr 5th, at 11:00",
       "reminder": true,
     }]
   )
 
+  // Delete tasks 
+  const deleteTask = (id) => {
+    console.log('delete', id)
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks  tasks={tasks}/>
+      <Tasks  tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,16 @@
-const Task = ({ task }) => {
+import { FaTimes } from 'react-icons/fa'
+
+const Task = ({ task, onDelete }) => {
   return (
     <div className="task">
-        <h3>{task.text}</h3>
+        <h3>
+            {task.text} 
+            <FaTimes
+                style={{ color: 'maroon', cursor: 'pointer'}}
+                onClick={onDelete}
+            />
+        </h3>
+        <p>{task.day}</p>
     </div>
   )
 }
